09ed1a3f66ece2a0262799b1851edd938887b711
David Bunger <david.1220.bunger@gmail.com>
2020-10-06 21:05:01 -0400

    rat

---
+++ backups/rat.js
@@ -0,0 +1,32 @@
+function loop(){
+    window.rat.update();
+    requestAnimationFrame(loop);
+}
+class Rat{
+    constructor(){
+        this.canvasElm = document.createElement("canvas");
+        this.canvasElm.width = 800;
+        this.canvasElm.height = 600;
+
+        this.gl = this.canvasElm.getContext("webgl2");
+        this.gl.clearColor(0.4,0.6,1.0,0.0);
+
+        document.body.appendChild(this.canvasElm);
+
+        let vs = document.getElementById("vs_01").innerHTML;
+		let fs = document.getElementById("fs_01").innerHTML;
+
+        this.sprite1 = new Sprite(this.gl, "img/ratimg.png", vs, fs);
+    }
+    update(){
+        this.gl.viewport(0,0, this.canvasElm.width, this.canvasElm.height);
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+
+        this.gl.enable(this.gl.BLEND);
+        this.gl.blendFunc(this.gl.SRC_ALPHA, this.gl.ONE_MINUS_SRC_ALPHA);
+
+        this.sprite1.render();
+
+        this.gl.flush();
+    }
+}
---
+++ index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname+"/public/index.html")
+})
+
+app.get('/js/:file', async (req, res) => {
+  res.sendFile(__dirname+`/js/${req.params.file}`)
+})
+
+app.get('/images/:file', async (req, res) => {
+  res.sendFile(__dirname+`/images/${req.params.file}`)
+})
+
+app.get('/suavemente', async (req, res) => {
+  res.sendFile(__dirname+`/audio/suave.mp3`);
+})
+
+app.get('*', (req, res) => {
+  res.send('NOT FOUND!!!!!');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
