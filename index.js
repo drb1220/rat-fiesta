33e8fbaa44ba6eaffecedacafcf35e3b32ac8324
David Bunger <david.1220.bunger@gmail.com>
2020-10-07 10:46:45 -0400

    fixed port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8080
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname+"/public/index.html")
