06785098756b1886c55cb239243a9f09be5bd845
David Bunger <david.1220.bunger@gmail.com>
2020-11-03 16:49:32 -0500

    gwa and gao

--- index.js
+++ index.js
@@ -19,6 +19,14 @@ app.get('/mcfuck', (req, res) => {
   res.sendFile(__dirname+"/public/mcride.html")
 })
 
+app.get('/gwagwa', (req, res) => {
+  res.sendFile(__dirname+"/public/gwagwa.html")
+})
+
+app.get('/gaogao', (req, res) => {
+  res.sendFile(__dirname+"/public/gaogao.html")
+})
+
 app.get('/js/:file', async (req, res) => {
   res.sendFile(__dirname+`/js/${req.params.file}`)
 })
@@ -41,6 +49,14 @@ app.get('/bts', async (req, res) => {
 
 app.get('/tiktakyon', async (req, res) => {
   res.sendFile(__dirname+`/audio/tiktakyon.mp3`);
+})
+
+app.get('/gwaaudio', async (req, res) => {
+  res.sendFile(__dirname+`/audio/gwagwa.mp3`);
+})
+
+app.get('/gaoaudio', async (req, res) => {
+  res.sendFile(__dirname+`/audio/gaogao.mp3`);
 })
 
 app.get('*', (req, res) => {
